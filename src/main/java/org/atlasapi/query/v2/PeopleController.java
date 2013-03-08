03da5effbb9b41f6656b0a7f1d07df4bd6bb07f2
Tom McAdam <tom@metabroadcast.com>
2013-03-08 18:16:13 +0000

    Update people controller to use PeopleQueryResolver

--- src/main/java/org/atlasapi/query/v2/PeopleController.java
+++ src/main/java/org/atlasapi/query/v2/PeopleController.java
@@ -10,19 +10,28 @@ import org.atlasapi.content.criteria.ContentQuery;
 import org.atlasapi.media.entity.Person;
 import org.atlasapi.output.AtlasErrorSummary;
 import org.atlasapi.output.AtlasModelWriter;
+import org.atlasapi.persistence.content.PeopleQueryResolver;
 import org.atlasapi.persistence.content.PeopleResolver;
 import org.atlasapi.persistence.logging.AdapterLog;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
+import com.metabroadcast.common.http.HttpStatusCode;
 
 @Controller
 public class PeopleController extends BaseController<Iterable<Person>> {
 
-    private final PeopleResolver resolver;
+    private static final AtlasErrorSummary NOT_FOUND = new AtlasErrorSummary(new NullPointerException())
+        .withErrorCode("Person not found")
+        .withStatusCode(HttpStatusCode.NOT_FOUND);
+    private static final AtlasErrorSummary FORBIDDEN = new AtlasErrorSummary(new NullPointerException())
+        .withStatusCode(HttpStatusCode.FORBIDDEN);
 
-    public PeopleController(PeopleResolver resolver, ApplicationConfigurationFetcher configFetcher,
+    private final PeopleQueryResolver resolver;
+
+    public PeopleController(PeopleQueryResolver resolver, ApplicationConfigurationFetcher configFetcher,
                     AdapterLog log, AtlasModelWriter<Iterable<Person>> outputter) {
         super(configFetcher, log, outputter);
         this.resolver = resolver;
@@ -38,9 +47,19 @@ public class PeopleController extends BaseController<Iterable<Person>> {
                 throw new IllegalArgumentException("No uri specified");
             }
             
-            Person person = resolver.person(uri).valueOrNull();
+            Optional<Person> person = resolver.person(uri, appConfig(request));
             
-            modelAndViewFor(request, response, ImmutableList.of(person), filter.getConfiguration());
+            if(!person.isPresent()) {
+                errorViewFor(request, response, NOT_FOUND);
+                return;
+            } 
+            
+            if(!appConfig(request).isEnabled(person.get().getPublisher())) {
+                errorViewFor(request, response, FORBIDDEN);
+                return;
+            }
+            
+            modelAndViewFor(request, response, ImmutableList.of(person.get()), filter.getConfiguration());
         } catch (Exception e) {
             errorViewFor(request, response, AtlasErrorSummary.forException(e));
         }
